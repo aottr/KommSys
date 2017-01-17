59bf8680c8e5edfee9495a83c794000f6835a6ad
Dustin Kröger <me@staubrein.org>
2017-01-17 01:23:25 +0100

    Hinzufügen der Modelklasse für ein Lied
    Datenmodell für Musikstücke (Interpret, Titel) mit Getter/Setter

---
+++ KSysteme/src/P2P/MusicModel.java
@@ -0,0 +1,62 @@
+package P2P;
+
+/**
+ *  Datenmodell eines Musikstückes zum Abspeichern von Titel und Interpret
+ *
+ *  @author Dustin Kröger [3728859]
+ *  @version 1.0
+ */
+public class MusicModel {
+
+    private String _artist;
+    private String _title;
+
+    /**
+     * Konstruktor des Datenmodells. Interpret und Titel werden übergeben und initial zugewiesen.
+     * @param artist Interpret des Musikstücks
+     * @param title Titel des Musikstücks
+     */
+    public MusicModel(String artist, String title) {
+
+        _artist = artist;
+        _title = title;
+    }
+
+    /**
+     * Getter des Interprets
+     * @return Interpret als String
+     */
+    public String getArtist() {
+
+        return _artist;
+    }
+
+    /**
+     * Getter des Titels
+     * @return Titel als String
+     */
+    public String getTitle() {
+
+        return _title;
+    }
+
+    /**
+     * Setter des Interprets
+     * @param value Interpret als String
+     */
+    public void setArtist(String value) {
+
+        // Behalten des alten Wertes falls leer
+        _artist = value.trim().equals("") ? _artist : value;
+    }
+
+    /**
+     * Setter des Titels
+     * @param value Titel als String
+     */
+    public  void setTitle(String value) {
+
+        // Behalten des alten Wertes falls leer
+        _title = value.trim().equals("") ? _title : value;
+    }
+}
