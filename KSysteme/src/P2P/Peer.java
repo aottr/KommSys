e365ebc42ba79813323dde3d41c58df132a0663e
Dustin Kröger <me@staubrein.org>
2017-01-18 15:50:38 +0100

    Dokumentation + IP-Getter

--- KSysteme/src/P2P/Peer.java
+++ KSysteme/src/P2P/Peer.java
@@ -2,19 +2,43 @@ package P2P;
 
 import java.net.*;
 
+/**
+ *  Datenmodell eines Peers zum Abspeichern von IP-Adresse und Port
+ *
+ *  @author Dustin Kröger [3728859]
+ *  @version 1.0
+ */
 public class Peer
 {
-    private InetAddress _ip;
-    private Integer _port;
+    private InetAddress ip;
+    private Integer port;
 
+    /**
+     * Konstruktor der Klasse.
+     * @param ip IP-Adresse des Peers
+     * @param port Portnummer des Peers
+     */
     public Peer(InetAddress ip, int port)
     {
-        _ip = ip;
-        _port = port;
+        this.ip = ip;
+        this.port = port;
     }
 
+    /**
+     * Getter des Ports
+     * @return Port des Peers
+     */
     public Integer getPort() {
 
-        return _port;
+        return port;
+    }
+
+    /**
+     * Getter der IP-Adresse
+     * @return IP-Adresse des Peers
+     */
+    public InetAddress getIPAddress() {
+
+        return ip;
     }
 }
